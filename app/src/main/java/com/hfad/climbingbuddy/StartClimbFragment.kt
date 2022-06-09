32666613a2c1bfdf46f75ea1fe8e83b97ca7ad51
Yasiregal9 <egalyasir@gmail.com>
2022-06-09 13:48:12 -0700

    Got the Chronometer working

--- app/src/main/java/com/hfad/climbingbuddy/StartClimbFragment.kt
+++ app/src/main/java/com/hfad/climbingbuddy/StartClimbFragment.kt
@@ -1,10 +1,12 @@
 package com.hfad.climbingbuddy
 
 import android.os.Bundle
+import android.os.SystemClock
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+<<<<<<< Updated upstream
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,9 +28,59 @@ class StartClimbFragment : Fragment() {
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
+=======
+import android.widget.Button
+import android.widget.Chronometer
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+
+class StartClimbFragment : Fragment() {
+
+
+    lateinit var TimeStart: Chronometer  //The stopwatch
+    var running = false  //Is the stopwatch running?
+    var offset: Long = 0
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        val view = inflater.inflate(R.layout.fragment_start_climb, container, false)
+        val homeButton = view.findViewById<Button>(R.id.homeButton1)
+        val startButton = view.findViewById<Button>(R.id.startButton)
+        val stopButton = view.findViewById<Button>(R.id.stopButton)
+        val saveButton = view.findViewById<Button>(R.id.saveButton)
+        val resetButton = view.findViewById<Button>(R.id.resetButton)
+        TimeStart = view.findViewById<Chronometer>(R.id.TimeStart)
+
+        homeButton.setOnClickListener {
+            view.findNavController()
+                .navigate(R.id.action_startClimbFragment_to_homeFragment)
+        }
+
+        startButton.setOnClickListener {
+
+            if (!running) {
+                setBaseTime()
+                TimeStart.start()
+                running = true
+            }
+        }
+
+        stopButton.setOnClickListener {
+            if (running) {
+                TimeStart.stop()
+                running = false
+            }
+        }
+
+        saveButton.setOnClickListener {
+            saveOffset()
+>>>>>>> Stashed changes
         }
     }
 
+<<<<<<< Updated upstream
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -55,5 +107,24 @@ class StartClimbFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+=======
+        resetButton.setOnClickListener {
+            offset = 0
+            setBaseTime()
+        }
+
+        //Inflate the layout for this fragment
+        return view
+>>>>>>> Stashed changes
+    }
+
+
+    fun setBaseTime() {
+        TimeStart.base = SystemClock.elapsedRealtime() - offset
+    }
+
+
+    fun saveOffset() {
+        offset = SystemClock.elapsedRealtime() - TimeStart.base
     }
 }
